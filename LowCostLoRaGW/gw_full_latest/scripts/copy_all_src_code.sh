dd8860bf4a5a2c26d6d2c9c4865017c1d9a78575
MicheledG <chiefzephyr@gmail.com>
2017-07-19 17:33:36 +0200

    Added utility script 'copy_all_src_code.sh'

---
+++ LowCostLoRaGW/gw_full_latest/scripts/copy_all_src_code.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#------------------------------------------------------------
+#
+# Simple script to automate the copy from the repo folder 
+# to the "lora_gateway" folder where the source code is compiled
+#
+# MicheledG 2017-07-19
+#
+#------------------------------------------------------------
+
+#
+# example: ./copy_all_src_code.sh
+
+echo "=================="
+echo "Replacing old source code with the new one"
+
+rmdir ../lora_gateway
+mkdir ../lora_gateway
+cd ../lora_gateway
+cp -R ../Tesi-LoRa/LowCostLoRaGW/gw_full_latest/* .
+
+echo "Done, bye!"
+echo "=================="
